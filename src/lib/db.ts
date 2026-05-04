f8bdd2b455072b8937a88b133b98234797309ed1
Vladimir Tishkin <lumeneux@Mac-mini-Vladimir.local>
2026-05-04 22:12:52 +0300

    db url change due to neon db location change

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,10 +6,10 @@ const globalForPrisma = globalThis as typeof globalThis & {
 };
 
 function createPrismaClient() {
-  const connectionString = process.env.EXCHANGE_STORAGE_DATABASE_URL;
+  const connectionString = process.env.XC_STORAGE_DATABASE_URL;
 
   if (!connectionString) {
-    throw new Error("EXCHANGE_STORAGE_DATABASE_URL is not configured.");
+    throw new Error("XC_STORAGE_DATABASE_URL is not configured.");
   }
 
   const adapter = new PrismaPg({ connectionString });
